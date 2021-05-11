fbd47a074e450f805fc1538a09f0bd67c62788cf
Knudsen2 <73479499+Knudsen2@users.noreply.github.com>
2021-05-11 09:27:01 +0200

    Overordnet ide

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Electronic</title>
+  </head>
+  <body>
+    <nav>
+      Her skal vi have to links - et der linker til forsiden og et til opret produkt siden - jeg tænker vi kan kopier denne nav bar så den kan benyttes på begge sider
+    </nav>
+
+    <main>
+      Her skal vi have visning af produkter
+    </main>
+  </body>
+</html>
---
+++ new_item.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Opret ny vare</title>
+  </head>
+  <body>
+    her skal vi have mulighed for at oprette en ny vare til vores database ved hjælp af PHP og html forms
+  </body>
+</html>
