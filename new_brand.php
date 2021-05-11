cc28805b60fe8cb24436458f21a105b5df5aaa4d
timahu <73479836+timahu@users.noreply.github.com>
2021-05-11 13:55:29 +0200

    Noget til brand

---
+++ new_brand.php
@@ -0,0 +1,35 @@
+<?php
+include('connect_db.php');
+$brand_name = null
+?>
+
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Create new brand</title>
+  </head>
+  <nav>
+    <a href="index.php">Homepage</a>
+    <br>
+  
+  </nav>
+  <body>
+
+    <form method="post">
+      <p>Create new brand</p>
+      <input type="text" name="brand" id="brands" placeholder="Name of new brand">
+      <button type="submit">Create</button>
+    </form>
+
+    <?php
+    if(isset($_POST['brand_name'])){
+      $brand_name = $_POST['brand_name'];
+      performQuery("INSERT INTO brands(brand) VALUES('$brand_name')");
+    }
+
+   ?>
+
+  </body>
+</html>
