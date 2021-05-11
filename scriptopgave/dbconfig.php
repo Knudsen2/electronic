2b45327632afdadab444954e8a747cf994ba8fc7
MikM95 <73488233+MikM95@users.noreply.github.com>
2021-05-11 12:32:28 +0200

    slettede tomme files

--- scriptopgave/dbconfig.php
+++
@@ -1,6 +0,0 @@
-<?php $con = mysqli_connect('localhost','root','root','electronics');
-if ($mysqli -> connect_errno) {
-  echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
-  exit();
-}
- ?>
