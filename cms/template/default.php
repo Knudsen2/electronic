d2bfe5e685e983b27d0610a75e36f7eeffbbd245
MikM95 <73488233+MikM95@users.noreply.github.com>
2021-05-11 12:51:53 +0200

    cleanup af filer

--- cms/template/default.php
+++
@@ -1,9 +0,0 @@
-<?php include('header.php'); ?>
-
-    <h2><?php echo $page['title']; ?></h2>
-
-    <p>
-      <?php echo $page['body']; ?>
-    </p>
-
-<?php include('footer.php'); ?>
--- scriptopgave/template/404.php
+++
@@ -1,5 +0,0 @@
-<?php include('header.php'); ?>
-
-    <h2> Siden findes desværre ikke</h2>
-
-<?php include('footer.php'); ?>
